12c3ec45a3a81dce6df6819230d8040daeafbdcb
James Tenrai <James@Jamess-MacBook-Pro.local>
2018-06-11 21:09:34 -1000

    first save

--- exercises.js
+++ exercises.js
@@ -8,7 +8,7 @@ Write a for-loop that will iterate through 20 numbers (starting at 1 and ending 
 .
 "Now serving 20."
 */
-
+//
 
 /* 2) Pop Charts
 Write a for-loop that will iterate through the topFive array below and console.log the following message:
